e913f61fb9c63458fb9d192059e1ef28d36777da
Fadil <rosunmungurudm@gmail.com>
2021-07-17 22:38:39 +0400

    custom LP

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,9 +6,13 @@ use App\Core\Controller;
 
 class HomeController extends Controller
 {
+    private const LP = 'hfx/lp.html.twig';
+
+    private const HP = 'home/index.html.twig';
+
     public function index()
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render(self::LP, [
             'txt_1' => 132465,
             'txt_2' => 'DEV',
         ]);
