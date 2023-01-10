eafbe36b6e40d438fc9b5057fbca75ecdce6ecc1
Fadil <rosunmungurudm@gmail.com>
2023-01-10 23:51:16 +0400

    added xdebug

--- www/phinx/seeds/UserSeeder.php
+++ www/phinx/seeds/UserSeeder.php
@@ -20,9 +20,9 @@ class UserSeeder extends AbstractSeed
         $data = [];
         for ($i=0; $i<25; $i++) :
             $data[] = [
-                'uuid'  => $faker->uuid,
-                'username' => $faker->companyEmail,
-                'name' => $faker->name,
+                'uuid'  => $faker->uuid(),
+                'username' => $faker->companyEmail(),
+                'name' => $faker->name(),
                 'password' => 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', # md5("admin")
                 'last_login' => $faker->date($format = 'Y-m-d', $max = 'now'),
             ];
