c6f5f263b25f15f29e5ed04586d16a23509e6c76
fadilxcoder <rosunmungurudm@gmail.com>
2025-11-17 23:39:56 +0400

    ci checker

---
+++ www/tests/Unit/QuickSmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class QuickSmokeTest extends TestCase
+{
+    public function testTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
